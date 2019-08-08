fb24fca86e66370e7f85bdb6908e27e621bb49ca
tim <tim@keynes.fr>
2019-08-08 17:56:30 +0800

    Email to user when payment is failed

--- api/app/Controllers/Http/StripeWebHookController.js
+++ api/app/Controllers/Http/StripeWebHookController.js
@@ -18,6 +18,9 @@ class StripeWebHookController {
       case 'customer.subscription.created':
         await this.newSubscription(request.post());
         break;
+      case 'invoice.payment_failed':
+        await this.paymentFailed(request.post());
+        break;
       default:
         console.log(`Webhook: ${request.post().type}: No handler`);
     }
@@ -65,6 +68,29 @@ class StripeWebHookController {
     }, message => {
       message.from(`${Env.get('EMAIL_SENDER_NAME')}<${Env.get('MAILGUN_EMAIL_SENDER')}>`);
       message.subject(`Welcome to${Env.get('APP_NAME')}`);
+      message.to(user.email);
+    });
+  }
+
+  async paymentFailed (data) {
+    const stripe_cus_id = data.data.object.customer;
+    let user;
+
+    if (stripe_cus_id === 'cus_00000000000000')
+      user = { email: Env.get('APP_SUPERADMIN_EMAIL') };
+    else user = await User.findBy('stripe_cus_id', stripe_cus_id);
+
+    Logger.info(`Webhook: ${user.email} ${data.type}`, data);
+
+    if (!user)
+      return;
+
+    await Mail.send('emails.notification', {
+      subject: 'Payment failed - Account suspended',
+      message: 'We\'re sorry, your payment have failed for some reasons and we\'re suspended your account... Please update your payments informations'
+    }, message => {
+      message.from(`${Env.get('EMAIL_SENDER_NAME')}<${Env.get('MAILGUN_EMAIL_SENDER')}>`);
+      message.subject(`Payment failed - Account suspended / ${Env.get('APP_NAME')}`);
       message.to(user.email);
     });
   }
