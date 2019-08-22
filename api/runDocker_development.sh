9aa3425651eb275a83f8839fee6d139e8194cdd6
tim <tim@keynes.fr>
2019-08-22 15:47:16 +0700

    Docker for API working with a volume

--- api/runDocker_development.sh
+++ api/runDocker_development.sh
@@ -7,4 +7,6 @@ docker build -t api.saastr .
 docker run  --name api.saastr \
             -p 3333:3333 \
             -e ENV_PATH=/app/.env.dev \
+            -e "NODE_ENV=development" \
+            -v $(pwd):/app \
             -d api.saastr
--- api/runDocker_production.sh
+++ api/runDocker_production.sh
@@ -7,4 +7,6 @@ docker build -t api.saastr .
 docker run  --name api.saastr \
             -p 3333:3333 \
             -e ENV_PATH=/app/.env \
+            -e "NODE_ENV=production" \
+            -v $(pwd):/app \
             -d api.saastr
