c818725ad228877651004fe0e1c9230325b56036
tim <tim@keynes.fr>
2019-08-22 13:39:09 +0700

    API docker support & cleaning

---
+++ api/runDocker_development.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker rm -f api.saastr
+
+docker build -t api.saastr .
+
+docker run  --name api.saastr \
+            -p 3333:3333 \
+            -e ENV_PATH=/app/.env.dev \
+            -d api.saastr
