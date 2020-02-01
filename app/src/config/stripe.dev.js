bb70954a86325640054264d151f81f6956e839f1
tim <tim@keynes.fr>
2020-02-01 12:10:42 +0100

    Comments on config files

--- app/src/config/stripe.dev.js
+++ app/src/config/stripe.dev.js
@@ -5,20 +5,31 @@ module.exports = {
     return this.pk_test;
   },
 
+  /**
+   * if yearly true, you offer yearly plans to users. Meaning you need for a single PLAN to define it on Stripe twice (for yearly and monthly) and below twice too.
+   * Exemple: GOLD_PLAN_YEARLY & GOLD_PLAN_MONTHLY
+   */
   yearly: true,
   plans: [
     {
+      // Need to be the ID you will find once you defined the plan on Stripe !
       id: 'plan_FS7wsGW6L2J9iI',
+      // See top comment about the monthly / yearly
       monthly: true,
       img: 'https://s21.postimg.cc/tpm0cge4n/space-ship.png',
       title: 'FREE',
       price: 'Free',
       features: ['DEDICATED', 'SIMPLE HORIZONTAL SCALABILITY'],
       freeTrial: true,
+      // Need to be the same as when you defined it on Stripe
       trialDays: 15,
+      // Visually featured !
       featured: false,
     },
     {
+      /**
+       * You can setup a FREE plan (only if you want..) ! Be sure you defined it as a 0€ PLAN on Stripe (Yes even if it's free)
+       */
       id: 'plan_FS7wsGW6L2J9iI',
       monthly: false,
       img: 'https://s21.postimg.cc/tpm0cge4n/space-ship.png',
